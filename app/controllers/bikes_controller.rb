61361112f78ee0f6f9093d9f6b2e0c6549dcb31a
AnneAriel Ceyrac <anneariel.ceyrac@gmail.com>
2022-11-22 11:35:52 +0100

    rattrapage pundit master

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -2,19 +2,31 @@ class BikesController < ApplicationController
 
   def index
     @bikes = Bike.all
+    @bikes = policy_scope(Bike)
+
   end
 
   def show
-  @bike = Bike.find(params[:id])
+    @bike = Bike.find(params[:id])
+    authorize @bike
   end
 
   def new
     @bike = Bike.new
+    authorize @bike
   end
 
   def create
-  @bike = Bike.new(bike_params)
-  @bike.save
+    @bike = Bike.new(bike_params)
+    @bike.save
+    authorize @bike
+  end
+
+  def destroy
+    authorize @bike
+    @bike = Bike.find(params[:id])
+    @bike.destrit
+    redirect_to restaurants_path, status: :see_other
   end
 
   private
