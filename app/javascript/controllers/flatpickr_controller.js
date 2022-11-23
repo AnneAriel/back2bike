b62afbec4d2ba2ed2011770bb96fe16d053d4271
Nelbi Leon <nelbi.leon@gmail.com>
2022-11-23 12:15:31 +0100

    added flatpickr and container to new resa

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,8 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
+import flatpickr from "flatpickr"
+
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
+  static targets = [ 'startDateInput', 'endDateInput' ]
   connect() {
+    console.log('coucu')
+    flatpickr(this.startDateInputTarget, {
+      mode: 'range',
+      "plugins": [new rangePlugin({ input: this.endDateInputTarget})]
+    })
   }
 }
